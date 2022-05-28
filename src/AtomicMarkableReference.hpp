7013a6b94a17f264da5c52cd0ae82e037a28c5c6
Luis Maya Aranda <mayafluis@gmail.com>
2022-05-28 00:48:42 -0700

    Repo refactoring continued

--- src/AtomicMarkableReference.hpp
+++ src/AtomicMarkableReference.hpp
@@ -1,71 +1,52 @@
-#include <atomic>
-#include <iostream>
+#pragma once
 
-/*
- * Struct for atomic markable reference
- */
+#include <atomic>
+
 template <class T>
 struct MarkableReference {
     T* next;
     bool marked;
 
-    MarkableReference() {
-        next = NULL;
-        marked = false;
-    }
+    MarkableReference() : next(nullptr), marked(false) {}
 
-    MarkableReference(T* node, bool mark) {
-        next = node;
-        marked = mark;
-    }
+    MarkableReference(T* node, bool mark) : next(node), marked(mark) {}
 
     bool operator==(const MarkableReference<T>& other) {
         return (next == other.next && marked == other.marked);
     }
 };
 
-/*
- * The AtomicMarkableReference class
- */
 template <class T>
 class AtomicMarkableReference {
    private:
-    /*
-		 * Since a pointer is always a POD type, even if T is of non-pod type
-		 * atomicity will be maintained
-		 */
+    // Since a pointer is always a POD type, even if T is of non-pod type
+    // atomicity will be maintained
     std::atomic<MarkableReference<T>*> markedNext;
 
    public:
     AtomicMarkableReference() {
-        markedNext.store(new MarkableReference<T>(NULL, false));
+        markedNext.store(new MarkableReference<T>(nullptr, false));
     }
 
     AtomicMarkableReference(T* nextNode, bool mark) {
-        /* Atomically store the values*/
+        // Atomically store the values
         markedNext.store(new MarkableReference<T>(nextNode, mark));
     }
 
-    /*
-		 * Returns the reference. load() is atomic and hence that will be the linearization point
-		 */
+    // Returns the reference. load() is atomic and hence that will be the linearization point
     T* getReference() {
         return markedNext.load()->next;
     }
 
-    /*
-		 * Returns the reference and update bool in the reference passed as the argument
-		 * load() is atomic and hence that will be the linearization point.
-		 */
+    // Returns the reference and update bool in the reference passed as the argument
+    // load() is atomic and hence that will be the linearization point.
     T* get(bool* mark) {
         MarkableReference<T>* temp = markedNext.load();
         *mark = temp->marked;
         return temp->next;
     }
 
-    /*
-		 * Set the variables unconditionally. load() is atomic and hence that will be the linearization point
-		 */
+    // Set the variables unconditionally. load() is atomic and hence that will be the linearization point
     void set(T* newRef, bool newMark) {
         MarkableReference<T>* curr = markedNext.load();
 
@@ -74,10 +55,8 @@ class AtomicMarkableReference {
         }
     }
 
-    /*
-		 * Tests an expected reference value and if the test succeeds,
-		 * replaces it with a new mark value
-		 */
+    // Tests an expected reference value and if the test succeeds,
+    // replaces it with a new mark value
     bool attemptMark(T* expected, bool newMark) {
         MarkableReference<T>* curr = markedNext.load();
 
@@ -88,12 +67,9 @@ class AtomicMarkableReference {
         return false;
     }
 
-    /*
-		 * CAS with reference and the marked field. load() is atomic and hence that will be the linearization point.
-		 * I take advantage of the fact that C++ has short-circuiting hence
-		 * if one of the first 2 conditions is false the atomic_compare_exchange_strong will not happen
-		 */
-
+    // CAS with reference and the marked field. load() is atomic and hence that will be the linearization point.
+    // Takes advantage of the fact that C++ has short-circuiting hence
+    // if one of the first 2 conditions is false the atomic_compare_exchange_strong will not happen
     bool CAS(T* expected, T* newValue, bool expectedBool, bool newBool) {
         MarkableReference<T>* curr = markedNext.load();
         return (expected == curr->next && expectedBool == curr->marked &&
--- src/FineGrainedLL.h
+++ src/FineGrainedLL.h
@@ -1,5 +1,6 @@
 /*************************************************************************
- * Luis Maya
+ * Luis Maya Aranda
+ *
  * Fine-Grained Synchronization Linked List
  * We can improve concurrency by locking individual nodes, rather than
  * locking the list as a whole. Instead of placing a lock on the entire list
@@ -7,243 +8,155 @@
  * each node when it first visits, and sometime later releases it. Such
  * fine-grained locking permits concurrent threads to traverse the list
  * together in a pipelined fashion.
+ *
  * **********************************************************************/
-#include <cstdlib>
+#pragma once
+
+#include <atomic>
 #include <iostream>
 #include <mutex>
 
-using namespace std;
-
 template <class T>
-class FineGrainedLL {
+class FineGrainedList {
    public:
-    FineGrainedLL();
-    ~FineGrainedLL();
-    bool contains(T);
-    bool add(T);
-    bool remove(T);
-    void printList();
-    void deleteList();
+    FineGrainedList();
+    ~FineGrainedList();
+    T front() const;
+    T back() const;
+    bool empty() const;
+    int size() const;
+    void push_back(const T &);
+    void pop_back();
+    void print_forwards();
+    void print_backwards();
 
    private:
     struct Node {
         T key;
+        Node *prev;
         Node *next;
-        mutex lock;
+        mutable std::mutex lock;
+
+        Node() : key(T()), prev(nullptr), next(nullptr) {}
+        Node(T key) : key(key), prev(nullptr), next(nullptr) {}
+        Node(T key, Node *prev, Node *next) : key(key), prev(prev), next(next) {}
     };
+    atomic<int> list_size;
     Node *head;
     Node *tail;
+    void delete_list();
 };
 
-/*************************************************************************
- * Initialize class variables
- * Head and tail will be used as a sentinel nodes
- * **********************************************************************/
 template <class T>
-FineGrainedLL<T>::FineGrainedLL() {
-    head = new Node;
-    head->key = {};
-
-    tail = new Node;
-    tail->key = {};
-    tail->next = NULL;
-
+FineGrainedList<T>::FineGrainedList() {
+    head = new Node();
+    tail = new Node();
     head->next = tail;
+    tail->prev = head;
 }
 
-/*************************************************************************
- * Deallocate linked list memory
- * **********************************************************************/
 template <class T>
-FineGrainedLL<T>::~FineGrainedLL() {
+FineGrainedList<T>::~FineGrainedList() {
     deleteList();
-
-    delete head;
-    delete tail;
 }
 
-/*************************************************************************
- * Uses fine grained locking to check if the given parameter is in
- * the linked list. If found, return true, else return false.
- * **********************************************************************/
 template <class T>
-bool FineGrainedLL<T>::contains(T key) {
-    // Acquire init(head) pred node lock
-    head->lock.lock();
-    Node *pred = head;
-
-    // Acquire curr node lock
-    pred->next->lock.lock();
-    Node *curr = head->next;
-
-    // While not at the of the linked list
-    while (curr != tail) {
-        // If current key is >= key, break out of traversal
-        if (curr->key >= key)
-            break;
-
-        // Release pred lock and update pred to curr
-        pred->lock.unlock();
-        pred = curr;
-
-        // Acquire next node lock and set curr to next node
-        curr->next->lock.lock();
-        curr = curr->next;
+T FineGrainedList<T>::front() const {
+    if (head->next) {
+        std::lock_guard<std::mutex> guard(head->next);
+        return head->next->key;
     }
-
-    // If key was not found, release pred and curr locks, and return false
-    if (curr != tail && curr->key != key) {
-        pred->lock.unlock();
-        curr->lock.unlock();
-        return false;
-    }
-
-    // If key was found, release pred and curr locks, and return true
-    pred->lock.unlock();
-    curr->lock.unlock();
-
-    return true;
+    return T();
 }
 
-/*************************************************************************
- * Uses fine grained locking to attempt to add the given parameter.
- * If the parameter is already in the linked list, do not add again and
- * return false. If parameter is not already in the linked list, add node
- * and return true.
- * **********************************************************************/
 template <class T>
-bool FineGrainedLL<T>::add(T key) {
-    // Acquire init(head) pred node lock
-    head->lock.lock();
-    Node *pred = head;
-
-    // Acquire curr node lock
-    pred->next->lock.lock();
-    Node *curr = head->next;
-
-    // While not at the of the linked list
-    while (curr != tail) {
-        // If current key is >= key, break out of traversal
-        if (curr->key >= key)
-            break;
-
-        // Release pred lock and update pred to curr
-        pred->lock.unlock();
-        pred = curr;
-
-        // Acquire next node lock and set curr to next node
-        curr->next->lock.lock();
-        curr = curr->next;
+T FineGrainedList<T>::back() const {
+    if (tail) {
+        std::lock_guard<std::mutex> guard(tail);
+        return tail->key;
     }
-
-    // If key is already inserted, release pred and curr locks, and return false
-    if (curr != tail && curr->key == key) {
-        pred->lock.unlock();
-        curr->lock.unlock();
-        return false;
-    }
-
-    // If key is not in the list, add node, release locks and return true
-    Node *node = new Node;
-    node->key = key;
-    node->next = curr;
-    pred->next = node;
-
-    pred->lock.unlock();
-    curr->lock.unlock();
-
-    return true;
+    return T();
 }
 
-/*************************************************************************
- * Uses fine grained locking to attempt to remove the give parameter.
- * If the parameter is not found in the linked list, return false. If the
- * parameter is found in the linked list, remove it and return true.
- * **********************************************************************/
 template <class T>
-bool FineGrainedLL<T>::remove(T key) {
-    // Acquire init(head) pred node lock
-    head->lock.lock();
-    Node *pred = head;
-
-    // Acquire curr node lock
-    pred->next->lock.lock();
-    Node *curr = head->next;
-
-    // While not at the of the linked list
-    while (curr != tail) {
-        // If current key is >= key, break out of traversal
-        if (curr->key >= key)
-            break;
-
-        // Release pred lock and update pred to curr
-        pred->lock.unlock();
-        pred = curr;
-
-        // Acquire next node lock and set curr to next node
-        curr->next->lock.lock();
-        curr = curr->next;
-    }
-
-    // If key was not found, release pred and curr locks, and return false
-    if (curr != tail && curr->key != key) {
-        pred->lock.unlock();
-        curr->lock.unlock();
-        return false;
-    }
-
-    // If key was found, remove it, release locks, and return true
-    //Node *temp;
-    //temp = curr;
-    pred->next = curr->next;
-    //delete temp;
-
-    pred->lock.unlock();
-    curr->lock.unlock();
-
-    return true;
+bool FineGrainedList<T>::empty() const {
+    return list_size == 0;
 }
 
-/*************************************************************************
- * Display contents of linked list
- * **********************************************************************/
 template <class T>
-void FineGrainedLL<T>::printList() {
-    // Acquire head lock
-    head->lock.lock();
-
-    // Set curr to head->next since head is a sentinel node
-    Node *curr = head->next;
-
-    // Traverse linked list and display contents
-    while (curr != tail) {
-        cout << curr->key << " ";
-        curr = curr->next;
-    }
-
-    // Release head lock
-    head->lock.unlock();
+int FineGrainedList<T>::size() const {
+    return list_size;
 }
 
-/*************************************************************************
- * Delete contents of linked list
- * **********************************************************************/
+
 template <class T>
-void FineGrainedLL<T>::deleteList() {
-    Node *temp;
+void FineGrainedList<T>::push_back(const T &key) {
+    Node *node = new Node(key);
+    std::scoped_lock<std::mutex> lock(head, tail);
 
-    while (head->next != tail) {
-        temp = head->next;
-        head->next = temp->next;
-        delete temp;
+    if (head->next == nullptr) {
+        head->next = node;
+        node->prev = head;
+        tail->prev = node;
+        node->next = tail;
+    } else {
+        tail->prev->next = node;
+        node->prev = tail->prev;
+        tail->prev = node;
+        node->next = tail;
     }
+    list_size++;
+}
 
-    head = new Node;
-    head->key = {};
+template <class T>
+void FineGrainedList<T>::pop_back() {
+    std::scoped_lock<std::mutex> lock(head, tail);
 
-    tail = new Node;
-    tail->key = {};
-    tail->next = NULL;
+    if (head->next && tail->prev) {
+        if (head->next == tail->prev) {
+            delete head->next;
+            head->next = nullptr;
+            tail- = nullptr;
+        } else {
+            Node *itr = tail;
+            tail = tail->prev;
+            tail->next = nullptr;
+            delete itr;
+        }
+        list_size--;
+    }
+}
 
-    head->next = tail;
+template <class T>
+void FineGrainedList<T>::print_forwards() {
+    Node *itr = head;
+    while (itr) {
+        std::cout << itr->key << " ";
+        itr = itr->next;
+    }
+    std::cout << std::endl;
+}
+
+template <class T>
+void FineGrainedList<T>::print_backwards() {
+    Node *itr = tail;
+    while (itr) {
+        std::cout << itr->key << " ";
+        itr = itr->prev;
+    }
+    std::cout << std::endl;
+}
+
+template <class T>
+void FineGrainedList<T>::delete_list() {
+    Node *itr = head;
+    while (itr) {
+        head = head->next;
+        head->prev = nullptr;
+        delete itr;
+        list_size--;
+        itr = head;
+    }
+    head = nullptr;
+    tail = nullptr;
 }
