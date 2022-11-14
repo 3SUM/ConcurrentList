ec0b83adf6626e385a8b69f35b4a2a02360e1bad
3SUM <mayafluis@gmail.com>
2022-11-14 13:30:30 -0800

    Refactoring coarse grained

--- src/CoarseGrainedList.hpp
+++ src/CoarseGrainedList.hpp
@@ -15,18 +15,6 @@
 
 template <class T>
 class CoarseGrainedList {
-   public:
-    CoarseGrainedList();
-    ~CoarseGrainedList();
-    T front() const;
-    T back() const;
-    bool empty() const;
-    int size() const;
-    void push_back(const T &);
-    void pop_back();
-    void print_forwards();
-    void print_backwards();
-
    private:
     struct Node {
         T key;
@@ -37,120 +25,92 @@ class CoarseGrainedList {
         Node(T key) : key(key), prev(nullptr), next(nullptr) {}
         Node(T key, Node *prev, Node *next) : key(key), prev(prev), next(next) {}
     };
-    int list_size;
     Node *head;
     Node *tail;
+    std::size_t list_size;
     mutable std::mutex lock;
-    void delete_list();
-};
+    void delete_list() {
+        std::lock_guard<std::mutex> guard(lock);
 
-template <class T>
-CoarseGrainedList<T>::CoarseGrainedList() {
-    list_size = 0;
-    head = nullptr;
-    tail = nullptr;
-}
-
-template <class T>
-CoarseGrainedList<T>::~CoarseGrainedList() {
-    delete_list();
-}
-
-template <class T>
-T CoarseGrainedList<T>::front() const {
-    std::lock_guard<std::mutex> guard(lock);
-    return head != nullptr ? head->key : T();
-}
-
-template <class T>
-T CoarseGrainedList<T>::back() const {
-    std::lock_guard<std::mutex> guard(lock);
-    return tail != nullptr ? tail->key : T();
-}
-
-template <class T>
-bool CoarseGrainedList<T>::empty() const {
-    std::lock_guard<std::mutex> guard(lock);
-    return head == nullptr && tail == nullptr;
-}
-
-template <class T>
-int CoarseGrainedList<T>::size() const {
-    std::lock_guard<std::mutex> guard(lock);
-    return list_size;
-}
-
-template <class T>
-void CoarseGrainedList<T>::push_back(const T &key) {
-    std::lock_guard<std::mutex> guard(lock);
-
-    Node *node = new Node(key);
-    if (head == nullptr) {
-        head = node;
-        tail = node;
-    } else {
-        tail->next = node;
-        node->prev = tail;
-        tail = node;
-    }
-    list_size++;
-}
-
-template <class T>
-void CoarseGrainedList<T>::pop_back() {
-    std::lock_guard<std::mutex> guard(lock);
-
-    if (head && tail) {
-        if (head == tail) {
-            delete head;
-            head = nullptr;
-            tail = nullptr;
-        } else {
-            Node *itr = tail;
-            tail = tail->prev;
-            tail->next = nullptr;
+        Node *itr = head;
+        while (itr) {
+            head = head->next;
+            head->prev = nullptr;
             delete itr;
+            list_size--;
+            itr = head;
         }
-        list_size--;
+        head = nullptr;
+        tail = nullptr;
     }
-}
 
-template <class T>
-void CoarseGrainedList<T>::print_forwards() {
-    std::lock_guard<std::mutex> guard(lock);
+   public:
+    CoarseGrainedList() : head(nullptr), tail(nullptr), list_size(0) {}
 
-    Node *itr = head;
-    while (itr) {
-        std::cout << itr->key << " ";
-        itr = itr->next;
+    ~CoarseGrainedList() {
+        delete_list();
     }
-    std::cout << std::endl;
-}
 
-template <class T>
-void CoarseGrainedList<T>::print_backwards() {
-    std::lock_guard<std::mutex> guard(lock);
-
-    Node *itr = tail;
-    while (itr) {
-        std::cout << itr->key << " ";
-        itr = itr->prev;
+    T front() const {
+        std::lock_guard<std::mutex> guard(lock);
+        return head != nullptr ? head->key : T();
     }
-    std::cout << std::endl;
-}
 
-template <class T>
-void CoarseGrainedList<T>::delete_list() {
-    std::lock_guard<std::mutex> guard(lock);
-
-    Node *itr = head;
-    while (itr) {
-        head = head->next;
-        head->prev = nullptr;
-        delete itr;
-        list_size--;
-        itr = head;
+    T back() const {
+        std::lock_guard<std::mutex> guard(lock);
+        return tail != nullptr ? tail->key : T();
     }
-    head = nullptr;
-    tail = nullptr;
-}
+
+    bool empty() const {
+        std::lock_guard<std::mutex> guard(lock);
+        return head == nullptr && tail == nullptr;
+    }
+
+    std::size_t size() const {
+        std::lock_guard<std::mutex> guard(lock);
+        return list_size;
+    }
+    void push_back(const T &key) {
+        std::lock_guard<std::mutex> guard(lock);
+
+        Node *node = new Node(key);
+        if (head == nullptr) {
+            head = node;
+            tail = node;
+        } else {
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+        }
+        list_size++;
+    }
+
+    void pop_back() {
+        std::lock_guard<std::mutex> guard(lock);
+
+        if (head && tail) {
+            if (head == tail) {
+                delete head;
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                Node *itr = tail;
+                tail = tail->prev;
+                tail->next = nullptr;
+                delete itr;
+            }
+            list_size--;
+        }
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const CoarseGrainedList &list) {
+        std::lock_guard<std::mutex> guard(list.lock);
+
+        Node *itr = list.head;
+        while (itr) {
+            os << itr->key << " ";
+            itr = itr->next;
+        }
+        return os;
+    }
+};
